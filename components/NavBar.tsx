4015bc0a405c8665f4d6b8d731aa03be61b20aeb
Tan Ziao <zi.ao.tan@outlook.com>
2022-10-27 19:56:06 +0800

    add the sidebar and the nav bar

---
+++ components/NavBar.tsx
@@ -0,0 +1,32 @@
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import React, { useEffect, useState } from "react";
+
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import Image from "next/image";
+import { IoMdAdd } from "react-icons/io";
+import Link from "next/link";
+// import useAuthStore from "../store/authStore";
+// import { IUser } from "../types";
+// import { createOrGetUser } from "../utils";
+import Logo from "../utils/tiktik-logo.png";
+import { useRouter } from "next/router";
+
+const NavBar = () => {
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="logo"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import { useEffect, useState } from "react";
+
+import type { AppProps } from "next/app";
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState<Boolean>(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) {
+    return null;
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <SideBar />
+        </div>
+        <div className="mt-4 flex flex-col  gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
 }
