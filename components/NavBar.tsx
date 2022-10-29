a1977370d96636b87c970c4f7a87692aee8dc0d2
Tan Ziao <zi.ao.tan@outlook.com>
2022-10-29 15:20:10 +0800

    add the search function

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,8 +13,18 @@ import useAuthStore from "../store/authStore";
 import { useRouter } from "next/router";
 
 const NavBar = () => {
-  const [user, setUser] = useState<IUser | null>(null);
+  const [user, setUser] = useState<IUser | null>();
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
   const { userProfile, addUser, removeUser } = useAuthStore();
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   useEffect(() => {
     setUser(userProfile);
@@ -35,17 +45,17 @@ const NavBar = () => {
 
       <div className="relative hidden md:block">
         <form
-          // onSubmit={handleSearch}
+          onSubmit={handleSearch}
           className="absolute md:static top-10 -left-20 bg-white"
         >
           <input
-            // value={searchValue}
-            // onChange={(e) => setSearchValue(e.target.value)}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
             className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
             placeholder="Search accounts and videos"
           />
           <button
-            // onClick={handleSearch}
+            onClick={handleSearch}
             className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
           >
             <BiSearch />
