4be9dd46eec46a184d0d270364019c09b1369367
Tan Ziao <zi.ao.tan@outlook.com>
2022-10-29 10:15:20 +0800

    add comment function

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,10 +1,21 @@
+import { BiCommentX } from "react-icons/bi";
+import { MdOutlineVideocamOff } from "react-icons/md";
 import React from "react";
 
 interface IProps {
   text: string;
 }
+
 const NoResults = ({ text }: IProps) => {
-  return <div>NoResults</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {/* <MdOutlineVideocamOff /> */}
+        <BiCommentX />
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
